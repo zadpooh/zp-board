68ba0f00148729d3f122a5e11db4ceda83c14b8d
zp <zadpooh@naver.com>
2017-11-02 13:08:46 +0900

    최초 커밋

--- src/main/java/com/dn/spring/user/UserManagerController.java
+++ src/main/java/com/dn/spring/user/UserManagerController.java
@@ -1,14 +1,11 @@
 package com.dn.spring.user;
 
+import com.dn.common.utils.ViewUtils;
+import com.dn.spring.annotations.RequestProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.dn.common.utils.ViewUtils;
-import com.dn.spring.annotations.CheckAuth;
-import com.dn.spring.annotations.RequestProperty;
-import com.dn.spring.user.domain.UserInfoRepository;
 
 @Controller
 @RequestMapping(value="/dnmanager/user")
@@ -17,9 +14,6 @@ public class UserManagerController {
 	
 	@Autowired
 	UserService userService;
-	
-	@Autowired
-    private UserInfoRepository userInfoRepository;
 	
 	@RequestMapping(value="/login")
 	@RequestProperty(layout="blank")
